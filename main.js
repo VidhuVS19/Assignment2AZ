768f1725235cff30d1f5f36755c75b9c1157f68a
VidhuVS19 <vidhu19122002@gmail.com>
2023-05-20 16:30:15 +0530

    Uni2

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-let flag = false;
-var image = document.getElementById("bulb");
-var src=image.src;
-var bulboff="file:///C:/Users/91987/Desktop/Webdev%20Learning%202023/Algozenith/Assignment%202/light-bulb-off.png"
-var bulbon="file:///C:/Users/91987/Desktop/Webdev%20Learning%202023/Algozenith/Assignment%202/light-bulb-on.png"
-function switchONBulb() {
-  image.src = bulbon;
-  src=image.src;
+var bulboff = document.querySelector("#bulboff")
+var bulbon = document.querySelector("#bulbon")
+var button = document.querySelector(".CircleButton")
+
+function switchON(){
+    bulboff.style.display='none'
+    bulbon.style.display='block'
 }
 
-function switchOFFBulb() {
-  image.src = bulboff;
-  src=image.src;
+function switchOFF(){
+    bulboff.style.display='block'
+    bulbon.style.display='none'
 }
 
-console.log(src);
-
-addEventListener("click", function() {
-  if (src === bulboff) {
-    switchONBulb();
-    flag = true;
-  } else {
-    switchOFFBulb();
-    flag = false;
-  }
-});
+button.addEventListener("click",function(){
+    if(bulbon.style.display === 'none'){
+        switchON()
+    }
+    else{
+        switchOFF()
+    }
 })
